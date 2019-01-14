b2acb3282a14bccefbad4386bcd4c47e77ed7389
Robert Cortez <Nuhuhfoo@gmail.com>
2019-01-13 22:32:56 -0800

    added pagination menu and rough draft of experience cards

--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -6,6 +6,9 @@ import "./index.css";
 import icon1 from '../src/assets/LinkedIn.png';
 import icon2 from '../src/assets/github.png';
 import icon3 from '../src/assets/Facebook.png';
+import arrow from '../src/assets/arrow.png';
+import {Card, CardTitle} from 'react-materialize'
+
 
 const Fullpage = () => (
     <ReactFullpage
@@ -19,19 +22,31 @@ const Fullpage = () => (
               <img className="icon" src={icon3} alt="logo" />
               <br></br>
               <span className="button" onClick={() => fullpageApi.moveSectionDown()}>
-              Swipe
+              <img className="vert-move" src={arrow} alt="logo" />
               </span>
             </div>
             <div className="section page2">
               <p>Experience</p>
-            </div>
+                    <div class="slide" data-anchor="slide1"> 
+                    <div>
+                    <Card class="card" header={<CardTitle reveal image={"img/office.jpg"} waves='light'/>}
+                        title="Card Title"reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}>
+    <                   p><a href="https://github.com/rcortez1775/MyPokeDex">This is a link</a></p>
+                    </Card>
+                    </div>
+                    </div>
+	                <div class="slide" data-anchor="slide2"> Slide 2 </div>
+	                <div class="slide" data-anchor="slide3"> Slide 3 </div>
+	                <div class="slide" data-anchor="slide4"> Slide 4 </div>
+                </div>
             <div className="section page3">
-              <p>About Me</p>
+            <p>About Me</p>
             </div>
           </ReactFullpage.Wrapper>
         );
       }}
     />
   );
+  
   
   ReactDOM.render(<Fullpage />, document.getElementById('react-root'));
