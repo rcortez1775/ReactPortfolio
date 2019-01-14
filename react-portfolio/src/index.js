c4b4c818e6dc4c805f59ae801876f968e14cffc2
Robert Cortez <Nuhuhfoo@gmail.com>
2019-01-13 18:59:45 -0800

    added social icons

--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import "fullpage.js/vendors/scrolloverflow"; // Optional. When using scrollOverflow:true
 import ReactFullpage from "@fullpage/react-fullpage";
 import "./index.css";
+import icon1 from '../src/assets/LinkedIn.png';
+import icon2 from '../src/assets/github.png';
+import icon3 from '../src/assets/Facebook.png';
 
 const Fullpage = () => (
     <ReactFullpage
@@ -11,6 +14,10 @@ const Fullpage = () => (
           <ReactFullpage.Wrapper>
             <div className="section page1">
               <p>Robert Cortez</p>
+              <img className="icon" src={icon1} alt="logo" />
+              <img className="icon" src={icon2} alt="logo" />
+              <img className="icon" src={icon3} alt="logo" />
+              <br></br>
               <span className="button" onClick={() => fullpageApi.moveSectionDown()}>
                 Click Me
               </span>
