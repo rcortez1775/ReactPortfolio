a6428c3d08c0fcbc6474abab2efa40fea61618b6
Robert Cortez <Nuhuhfoo@gmail.com>
2019-01-15 17:02:58 -0800

    updates

--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -33,45 +33,45 @@ const Fullpage = () => (
                     <div className="slide pokeslide" data-anchor="slide1"> 
                     <img src={poke} alt="ash" className="ash"></img>
                     <div className="pokedex">
-                    <p className="title">The App:</p>
-                    <p className="title">The Code:</p>
-                    <p className="title">Technologies:</p>
+                    <p className="title">The App: https://mypokedex1.herokuapp.com/</p>
+                    <p className="title">The Code: https://github.com/rcortez1775/MyPokeDex</p>
+                    <p className="title">Technologies: React, Node.js and Material-UI</p>
                     </div>
                     </div>
 
 	                <div className="slide" data-anchor="slide2"> 
                     <img src={codecard} alt="code" className="ash"></img>
                     <div className="pokedex">
-                    <p className="title">The App:</p>
-                    <p className="title">The Code:</p>
-                    <p className="title">Technologies:</p>
+                    <p className="title">The App: https://my-code-card.herokuapp.com/</p>
+                    <p className="title">The Code: https://github.com/The-Gummy-Bears/CodeCards</p>
+                    <p className="title">Technologies: MongoDB, Express, React, Node.js and Material-UI</p>
                     </div>
                     </div>
 
 	                <div className="slide" data-anchor="slide3">
                     <img src={circle} alt="code" className="ash"></img>
                     <div className="pokedex">
-                    <p className="title">The App:</p>
-                    <p className="title">The Code:</p>
-                    <p className="title">Technologies:</p>
+                    <p className="title">The App: https://circle-of-friends.herokuapp.com/</p>
+                    <p className="title">The Code: https://github.com/The-Real-Tardigrades/Contextual-Geolocation</p>
+                    <p className="title">Technologies: MySql, Express, Node.js, Javascript/jQuery and Materialize</p>
                     </div>
                     </div>
 
 	                <div className="slide" data-anchor="slide4">
                     <img src={list} alt="list" className="ash"></img>
                     <div className="pokedex">
-                    <p className="title">The App: </p>
-                    <p className="title">The Code:</p>
-                    <p className="title">Technologies:</p>
+                    <p className="title">The App: https://rcortez1775.github.io/MyToDoList/</p>
+                    <p className="title">The Code: https://github.com/rcortez1775/MyToDoList</p>
+                    <p className="title">Technologies: Javascript</p>
                     </div>
                     </div>
 
                     <div className="slide" data-anchor="slide5">
                     <img src={earth} alt="earth" className="ash"></img>
                     <div className="pokedex">
-                    <p className="title">The App: </p>
-                    <p className="title">The Code:</p>
-                    <p className="title">Technologies:</p>
+                    <p className="title">The App: https://rcortez1775.github.io/project1/</p>
+                    <p className="title">The Code: https://github.com/rcortez1775/project1</p>
+                    <p className="title">Technologies: Javascript and Google API</p>
                     </div>
                     </div>
                 </div>
