1264de1195784f22bf17b3b6f401f86e8ec4fdc0
Robert Cortez <Nuhuhfoo@gmail.com>
2019-01-30 10:12:12 -0800

    fixed project app links

--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -34,8 +34,8 @@ const Fullpage = () => (
                     <div className="slide pokeslide" data-anchor="slide1"> 
                     <img src={poke} alt="ash" className="ash"></img>
                     <div className="pokedex">
-                    <p className="title">The App: https://mypokedex1.herokuapp.com/</p>
-                    <p className="title">The Code: https://github.com/rcortez1775/MyPokeDex</p>
+                    <p className="title">The App: <span onClick={()=> window.open("https://mypokedex1.herokuapp.com/", "_blank")}>Click Me!</span></p>
+                    <p className="title">The Code: <span onClick={()=> window.open("https://github.com/rcortez1775/MyPokeDex", "_blank")}>Click Me!</span></p>
                     <p className="title">Technologies: React, Node.js and Material-UI</p>
                     </div>
                     </div>
@@ -43,8 +43,8 @@ const Fullpage = () => (
 	                <div className="slide" data-anchor="slide2"> 
                     <img src={codecard} alt="code" className="ash"></img>
                     <div className="pokedex">
-                    <p className="title">The App: https://my-code-card.herokuapp.com/</p>
-                    <p className="title">The Code: https://github.com/The-Gummy-Bears/CodeCards</p>
+                    <p className="title">The App: <span onClick={()=> window.open("https://my-code-card.herokuapp.com/", "_blank")}>Click Me!</span></p>
+                    <p className="title">The Code: <span onClick={()=> window.open("https://github.com/The-Gummy-Bears/CodeCards", "_blank")}>Click Me!</span></p>
                     <p className="title">Technologies: MongoDB, Express, React, Node.js and Material-UI</p>
                     </div>
                     </div>
@@ -52,8 +52,8 @@ const Fullpage = () => (
 	                <div className="slide" data-anchor="slide3">
                     <img src={circle} alt="code" className="ash"></img>
                     <div className="pokedex">
-                    <p className="title">The App: https://circle-of-friends.herokuapp.com/</p>
-                    <p className="title">The Code: https://github.com/The-Real-Tardigrades/Contextual-Geolocation</p>
+                    <p className="title">The App: <span onClick={()=> window.open("https://circle-of-friends.herokuapp.com/", "_blank")}>Click Me!</span></p>
+                    <p className="title">The Code: <span onClick={()=> window.open("https://github.com/The-Real-Tardigrades/Contextual-Geolocation", "_blank")}>Click Me!</span></p>
                     <p className="title">Technologies: MySql, Express, Node.js, Javascript/jQuery and Materialize</p>
                     </div>
                     </div>
@@ -61,8 +61,8 @@ const Fullpage = () => (
 	                <div className="slide" data-anchor="slide4">
                     <img src={list} alt="list" className="ash"></img>
                     <div className="pokedex">
-                    <p className="title">The App: https://rcortez1775.github.io/MyToDoList/</p>
-                    <p className="title">The Code: https://github.com/rcortez1775/MyToDoList</p>
+                    <p className="title">The App: <span onClick={()=> window.open("https://rcortez1775.github.io/MyToDoList/", "_blank")}>Click Me!</span></p>
+                    <p className="title">The Code: <span onClick={()=> window.open("https://github.com/rcortez1775/MyToDoList", "_blank")}>Click Me!</span></p>
                     <p className="title">Technologies: Javascript</p>
                     </div>
                     </div>
@@ -70,8 +70,8 @@ const Fullpage = () => (
                     <div className="slide" data-anchor="slide5">
                     <img src={earth} alt="earth" className="ash"></img>
                     <div className="pokedex">
-                    <p className="title">The App: https://rcortez1775.github.io/project1/</p>
-                    <p className="title">The Code: https://github.com/rcortez1775/project1</p>
+                    <p className="title">The App: <span onClick={()=> window.open("https://rcortez1775.github.io/project1/", "_blank")}>Click Me!</span></p>
+                    <p className="title">The Code: <span onClick={()=> window.open("https://github.com/rcortez1775/project1", "_blank")}>Click Me!</span></p>
                     <p className="title">Technologies: Javascript and Google API</p>
                     </div>
                     </div>
